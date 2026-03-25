85ba70becd425b975bb01fc236285a77baa26cd9
DCx7C5 <dcxdevelopment@protonmail.com>
2026-03-25 01:58:11 +0100

    feat(action): support multi-package and multi-key operations
    - Enhance action inputs to accept new-line separated lists for
      package names, keys, and values, enabling batch operations
    - Add 'output-structure' input for flexible output formatting
    - Refactor action logic to centralize all operations in a single
      'run_action' step
    - Introduce libpackages.sh to generate jq filters for complex
      package manipulations
    - Remove obsolete steps and streamline JSON handling
    
    Signed-off-by: DCx7C5 <dcxdevelopment@protonmail.com>

---
+++ aur-json-packages/libpackages.sh
@@ -0,0 +1,224 @@
+#!/usr/bin/env bash
+
+
+ARGS=()
+FILTER=".packages"
+
+
+comma_separated() {
+  printf '%s\n' "$1" | tr "\n" "," | sed -e 's/,$//'
+}
+
+INPUT_ACTION="$1"
+INPUT_PKGNAMES="$2"
+INPUT_KEYS="$3"
+INPUT_VALUES="$4"
+IS_JSON_VALUE="${5:-false}"
+
+[[ "$IS_JSON_VALUE" == "true" ]] && INPUT_VALUES=$(pint "$INPUT_VALUES" | jq -c .) || INPUT_VALUES=$(comma_separated "$INPUT_VALUES")
+
+readarray -t INPUT_KEYS < <(printf '%s\n' "$INPUT_KEYS")
+readarray -t INPUT_PKGNAMES < <(printf '%s\n' "$INPUT_PKGNAMES")
+readarray -t INPUT_VALUES < <(printf '%s\n' "$INPUT_VALUES")
+if [[ "$INPUT_ACTION" =~ ^(add|update|remove|delete)$ ]]; then
+  ARGS+=(set)
+fi
+
+array_contains_not() {
+  local search="$1"; shift
+  local item
+  for item in "$@"; do
+    [[ "$item" == "$search" ]] && return 1
+  done
+  return 0
+}
+
+is_json() {
+  local input="$1"
+  [[ -z "$input" ]] && return 1
+  echo "$input" | jq -e . >/dev/null 2>&1
+}
+
+
+get_packages_with_pkgname_and_keys() {
+  local filter="$FILTER | {"
+  local args=()
+  local n=0 i=0
+  for ((i=0; i<${#INPUT_PKGNAMES[@]}; i++)); do
+    pkgname="${INPUT_PKGNAMES[i]}"
+    filter+="\$pkg$i: {"
+    for ((n=0; n<${#INPUT_KEYS[@]}; n++)); do
+      key="${INPUT_KEYS[n]}"
+      filter+="\$key$n: .[\$pkg$i][\$key$n],"
+      arg="--arg key$n $key"
+      array_contains_not "$arg" "${args[@]}" && args+=("$arg")
+    done
+    args+=(--arg "pkg$i" "$pkgname")
+    filter="${filter%,}"
+    filter+="},"
+  done
+  filter="${filter%,}"
+  filter+="}"
+  printf '%s %s\n' "${args[*]}" "'$filter'"
+}
+
+get_all_packages_with_keys() {
+  local filter="$FILTER | map_values({"
+  local args=()
+  for ((n=0; n<${#INPUT_KEYS[@]}; n++)); do
+    key="${INPUT_KEYS[n]}"
+    args+=(--arg "key$n" "$key")
+    filter+="\$key$n: .[\$key$n],"
+  done
+  filter="${filter%,}"
+  filter+="})"
+  printf '%s %s\n' "${args[*]}" "'$filter'"
+}
+
+get_packages_with_pkgnames() {
+  local filter="$FILTER | {"
+  local args=()
+  for ((i=0; i<${#INPUT_PKGNAMES[@]}; i++)); do
+    pkgname="${INPUT_PKGNAMES[i]}"
+    args+=(--arg "pkg$i" "$pkgname")
+    filter+="\$pkg$i: .[\$pkg$i],"
+  done
+  filter=$(printf '%s\n' "$filter" | sed -e 's/,$//')
+  filter+="}"
+  printf '%s %s\n' "${args[*]}" "'$filter'"
+}
+
+get_all_packages() {
+  local filter="$FILTER"
+  printf '%s\n' "'$filter'"
+}
+
+add_package_with_multiple_pkgnames_and_multiple_values_and_no_keys() {
+  local args=()
+  local filter="$FILTER + {"
+  for ((i=0; i<${#INPUT_PKGNAMES[@]}; i++)); do
+    pkgname="${INPUT_PKGNAMES[i]}"
+    value="${INPUT_VALUES[i]}"
+    args+=(--arg "pkg$i" "$pkgname")
+    args+=(--argjson "val$i" "'$value'")
+    filter+="\$pkg$i: \$val$i,"
+  done
+  filter="${filter%,}"
+  filter+="}"
+  printf '%s %s\n' "${args[*]}" "'$filter'"
+}
+
+add_package_with_one_pkgname_and_multiple_keys_and_multiple_values() {
+  local args=()
+  local filter="$FILTER + {"
+  pkgname="${INPUT_PKGNAMES[0]}"
+  args+=(--arg "pkg" "$pkgname")
+  filter+="\$pkg: {"
+  for ((i=0; i<${#INPUT_KEYS[@]}; i++)); do
+    key="${INPUT_KEYS[i]}"
+    value="${INPUT_VALUES[i]}"
+    args+=(--arg "key$i" "$key")
+    args+=(--arg "val$i" "$value")
+    filter+="\$key$i: \$val$i,"
+  done
+  filter="${filter%,}"
+  filter+="}}"
+  printf '%s %s\n' "${args[*]}" "'$filter'"
+}
+
+add_package_with_no_pkgname_and_multiple_values_and_no_keys() {
+  local args=()
+  local filter="$FILTER + "
+  for ((i=0; i<${#INPUT_VALUES[@]}; i++)); do
+    value="${INPUT_VALUES[i]}"
+    args+=(--argjson "val$i" "'$value'")
+    filter+="\$val$i,"
+  done
+  filter="${filter%,}"
+  printf '%s %s\n' "${args[*]}" "'$filter'"
+}
+
+
+remove_packages_with_multiple_pkgnames() {
+  local args=()
+  local filter="$FILTER | del("
+  for ((i=0; i<${#INPUT_PKGNAMES[@]}; i++)); do
+    pkgname="${INPUT_PKGNAMES[i]}"
+    args+=(--arg "pkg$i" "$pkgname")
+    filter+=".[\$pkg$i],"
+  done
+  filter="${filter%,}"
+  filter+=")"
+  printf '%s %s\n' "${args[*]}" "'$filter'"
+}
+
+remove_from_one_package_multiple_keys() {
+  local args=()
+  local filter="$FILTER |= (.[\$pkg] |= del("
+  pkgname="${INPUT_PKGNAMES[0]}"
+  args+=(--arg "pkg" "$pkgname")
+  for ((i=0; i<${#INPUT_KEYS[@]}; i++)); do
+    key="${INPUT_KEYS[i]}"
+    args+=(--arg "key$i" "$key")
+    filter+=".[\$key$i],"
+  done
+  filter="${filter%,}"
+  filter+="))"
+  printf '%s %s\n' "${args[*]}" "'$filter'"
+}
+
+action_get() {
+  # pkgnames and keys
+  if [[ -z "$1" && -z "$2" ]]; then
+    get_all_packages
+  elif [[ -n "$1" && -n "$2" ]]; then
+    get_packages_with_pkgname_and_keys
+  elif [[ -n "$1" && -z "$2" ]]; then
+    get_packages_with_pkgnames
+  elif [[ -z "$1" && -n "$2" ]]; then
+    get_all_packages_with_keys
+  else
+    echo "Invalid input combination for 'get' action." >&2
+    exit 1
+  fi
+}
+
+action_update() {
+  if [[ -n "$1" && -n "$2" ]]; then
+    add_package_with_one_pkgname_and_multiple_keys_and_multiple_values
+  elif [[ -z "$1" && -n "$2" ]]; then
+    add_package_with_no_pkgname_and_multiple_values_and_no_keys
+  elif [[ -n "$1" && -z "$2" ]]; then
+    add_package_with_multiple_pkgnames_and_multiple_values_and_no_keys
+  else
+    echo "Invalid input combination for 'update' action." >&2
+    exit 1
+  fi
+}
+
+action_remove() {
+  if [[ -n "$1" && -z "$2" ]]; then
+    remove_packages_with_multiple_pkgnames
+  elif [[ -n "$1" && -n "$2" ]]; then
+    remove_from_one_package_multiple_keys
+  else
+    echo "Invalid input combination for 'remove' action." >&2
+    exit 1
+  fi
+}
+
+case "$INPUT_ACTION" in
+  get)
+    action_get "$2" "$3"
+    ;;
+  update|add)
+    action_update "$2" "$3"
+    ;;
+  remove|delete)
+    action_remove "$2" "$3"
+    ;;
+  *)
+    echo "Invalid action: $INPUT_ACTION. Supported actions are: get, add, update, remove, delete." >&2
+    exit 1
+    ;;
+esac
