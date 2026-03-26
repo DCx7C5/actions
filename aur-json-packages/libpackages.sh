9f4773ecf85dd26e64911ab221c8930d5f503230
DCx7C5 <dcxdevelopment@protonmail.com>
2026-03-26 14:30:37 +0100

    fix(libpackages): correct array index in input key check
    Signed-off-by: DCx7C5 <dcxdevelopment@protonmail.com>

--- aur-json-packages/libpackages.sh
+++ aur-json-packages/libpackages.sh
@@ -48,7 +48,7 @@ get_all_packages_with_keys() {
   local filter="$FILTER | map_values({"
   local args=()
   for ((n=0; n<${#INPUT_KEYS[@]}; n++)); do
-    [[ -z "${INPUT_KEYS[i]}" ]] && continue
+    [[ -z "${INPUT_KEYS[n]}" ]] && continue
     key="${INPUT_KEYS[n]}"
     args+=(--arg "key$n" "$key")
     filter+="\$key$n: .[\$key$n],"
