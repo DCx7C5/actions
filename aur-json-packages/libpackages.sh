85597ac735872859fb668d181b53b506de8f405b
DCx7C5 <dcxdevelopment@protonmail.com>
2026-04-02 12:22:59 +0200

    fix(workflow): correct JSON value handling in package arguments
    Ensures proper formatting of JSON values when passing to the filter.
    This change prevents potential errors in value parsing and improves
    the reliability of the workflow.
    
    Signed-off-by: DCx7C5 <dcxdevelopment@protonmail.com>

--- aur-json-packages/libpackages.sh
+++ aur-json-packages/libpackages.sh
@@ -98,7 +98,7 @@ add_package_with_multiple_pkgnames_and_multiple_values_and_no_keys() {
     [[ -z "${INPUT_PKGNAMES[i]}" || -z "${INPUT_VALUES[i]}" ]] && continue
     [[ "$IS_JSON_VALUE" == "true" ]] && value="$(jq -c 'fromjson' <<< "${INPUT_VALUES[i]}")" || value="${INPUT_VALUES[i]}"
     args+=(--arg "pkg$i" "${INPUT_PKGNAMES[i]}")
-    args+=(--argjson "val$i" "$value")
+    args+=(--argjson "val$i" "'$value'")
     filter+="\$pkg$i: \$val$i,"
   done
   filter="${filter%,}}"
